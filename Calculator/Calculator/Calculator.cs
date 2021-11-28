d30c5beba880ad9198e1f13eab29293a845af271
Wajiha Fatima <wafa022001@gmail.com>
2021-11-28 18:42:16 +0500

    Commited

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator_1
+{
+    public class Calculator
+    {
+        public int n1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int n2
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void sub()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void multiply()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void divide()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class CopyOfCalculator
+    {
+        public int n1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int n2
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void sub()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void multiply()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void divide()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+class Calculator
+{
+
+    public int ans;
+    
+
+    public int add(int n1, int n2)
+    {
+        ans = n1 + n2;
+        return ans;
+    }
+
+    public int sub(int n1, int n2)
+    {
+        ans = n1 - n2;
+        return ans; }
+
+    public int divide(int n1, int n2)
+    {
+        try
+        {
+            ans = n1 / n2;
+            return ans;
+        }
+        
+        catch (DivideByZeroException)
+        {
+
+            ans = 0;
+            return ans;
+        }
+        }
+
+    public int multiply(int n1, int n2)
+    {
+        ans = n1 * n2;
+        return ans; }
+   
+  
+   static void Main(string[] args)
+    {
+        Calculator cal = new Calculator();
+        Console.WriteLine("Enter first number : ");
+       int n1 =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("Enter second number : ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine("Choose operation: ");
+        Console.WriteLine(" ");
+        Console.WriteLine("Addition (add) ");
+        Console.WriteLine("Subtraction (sub) ");
+        Console.WriteLine("Multiplication (mul) ");
+        Console.WriteLine("Division (div) ");
+        string op=Console.ReadLine();
+        Console.WriteLine(" ");
+
+        if (op == "add") {
+            cal.add(n1, n2);
+        }
+
+      if (op == "sub")
+        {
+            cal.sub(n1, n2);
+        }
+
+        if (op == "div")
+        {
+            cal.divide(n1,n2);
+
+          }
+
+        if (op == "mul")
+        {
+            cal.multiply(n1, n2);
+        }
+
+        Console.WriteLine(n1+" " + op + " " + n2 + " = " + cal.ans);
+    }
+   
+}
+
